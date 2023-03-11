a822a869c79a7deb97cb0ae5883d9c7ccc9123a9
yasmin afifi <yasminkhaledd2@gmail.com>
2023-03-11 23:35:03 +0200

    Create Platform_Types.h

---
+++ Platform_Types.h
@@ -0,0 +1,55 @@
+ /******************************************************************************
+ *
+ * Module: Common - Platform Abstraction
+ *
+ * File Name: Platform_Types.h
+ *
+ * Description: Platform types 
+ *
+ *******************************************************************************/
+
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+
+#define PLATFORM_VENDOR_ID                          (1000U)
+
+/*
+ * Module Version 1.0.0
+ */
+#define PLATFORM_SW_MAJOR_VERSION                   (1U)
+#define PLATFORM_SW_MINOR_VERSION                   (0U)
+#define PLATFORM_SW_PATCH_VERSION                   (0U)
+
+/*
+ * AUTOSAR Version 4.0.3
+ */
+#define PLATFORM_AR_RELEASE_MAJOR_VERSION           (4U)
+#define PLATFORM_AR_RELEASE_MINOR_VERSION           (0U)
+#define PLATFORM_AR_RELEASE_PATCH_VERSION           (3U)
+
+
+/*
+ * Boolean Values
+ */
+#ifndef FALSE
+#define FALSE       (0u)
+#endif
+#ifndef TRUE
+#define TRUE        (1u)
+#endif
+
+typedef unsigned char         boolean;
+
+typedef unsigned char         uint8;          /*           0 .. 255             */
+typedef signed char           sint8;          /*        -128 .. +127            */
+typedef unsigned short        uint16;         /*           0 .. 65535           */
+typedef signed short          sint16;         /*      -32768 .. +32767          */
+typedef unsigned long         uint32;         /*           0 .. 4294967295      */
+typedef signed long           sint32;         /* -2147483648 .. +2147483647     */
+typedef unsigned long long    uint64;         /*       0..18446744073709551615  */
+typedef signed long long      sint64;         /* -9223372036854775808 .. 9223372036854775807 */
+typedef float                 float32;
+typedef double                float64;
+
+#endif /* PLATFORM_TYPES_H */

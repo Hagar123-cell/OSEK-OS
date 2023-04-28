87d5a6ebfc08dc0b3f590a7fd96e6dcf8a705017
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2023-04-29 01:23:47 +0200

    Add files via upload

---
+++ scheduler.c
@@ -0,0 +1,135 @@
+/******************************************************************************
+ *
+ * Module: scheduler
+ *
+ * File Name: scheduler.c
+ *
+ * Description: Source file for scheduler
+ *
+ * Author:
+ *
+ *******************************************************************************/
+
+#include "scheduler.h"
+OsTask_TCBType              	OsTask_TCBs        [OSTASK_NUMBER]; /* TCB array for tasks based on task ID */
+static Os_ReadyListType             OsTask_ReadyList           [OSTASK_PRIORITY_LEVELS]; /* array of ready lists based on priority */
+static TaskType                     OsTask_RunningTask;                         /* currently running task */
+static TaskType                     OsTask_RunningTaskNext;                    /*      task to be run next     */
+
+
+/*******************************************************************************
+*Function Name: OsSched_ReadyListRemoveFromHead
+*Parameter (In): list  	    -ready list to remove task from.
+*Parameter (Out): none
+*Parameter (In/Out): none
+*Return : none
+*Description: Remove the task at the head of the list .
+********************************************************************************/
+static void OsSched_ReadyListRemoveFromHead(
+											Os_ReadyListType* list
+										   )
+{
+	TaskType oldHead;
+
+	if(list->head == INVALID_TASK) /* list is already empty */
+	{
+		/* Do Nothing */
+	}
+	else if (list->head == list->tail ) /* there is only one task in list */
+	{
+		list->head = INVALID_TASK;
+		list->tail = INVALID_TASK;
+		/* Removed task next is already INVALID_TASK */
+	}
+	else  /* list contains more than one task */
+	{
+		oldHead=list->head;
+		list->head=OsTask_TCBs[oldHead].next;
+		OsTask_TCBs[oldHead].next=INVALID_TASK; /* assign Removed task next to INVALID_TASK */
+	}
+}
+
+
+
+
+/*******************************************************************************
+*Function Name: OsSched_RunningToReady
+*Parameter (In): taskID		-id of the task to be moved from running to ready.
+*Parameter (Out): none
+*Parameter (In/Out): none
+*Return : none
+*Description: change task state from running to ready,
+*			  add the task to the head of the ready list.
+*********************************************************************************/
+void OsSched_RunningToReady(
+							TaskType taskID
+							)
+{
+    OsTask_TCBs[taskID].state = READY;
+    OsSched_ReadyListAddToHead(&OsTask_ReadyList[OsTask_TCBs[taskID].CurrentPriority], taskID);
+}
+
+
+/*******************************************************************************
+*Macro Name: OsSched_SuspendedToReady
+*Parameter (In): taskID		-id of the task to be moved from Suspended to ready.
+*Parameter (Out): none
+*Parameter (In/Out): none
+*Return : none
+*Description: change task state from Suspended to ready,
+*			  add the task to the tail of the ready list.
+*********************************************************************************/
+void OsSched_SuspendedToReady(
+								TaskType taskID
+								)
+{
+/*
+ * task release all resources and internal resources before termination (move to suspended state),
+ * so its current priority returns to the base priority set at configuration .
+ */
+#if 0
+	OsTaskPriorityType priority;
+    priority =OsTask_TCBs[taskID].OsTaskConfig->OsTaskPriority;
+    OsTask_TCBs[taskID].CurrentPriority = priority;
+#endif
+    OsSched_ReadyListAddToTail(&OsTask_ReadyList[OsTask_TCBs[taskID].CurrentPriority], taskID);
+    OsTask_TCBs[taskID].state    = READY; /* change state of the task to ready*/
+}
+
+/*******************************************************************************
+*Function Name: OsSched_ReadyToRunning
+*Parameter (In): taskID		-id of the task to be moved from ready to running.
+*Parameter (Out): none
+*Parameter (In/Out): none
+*Return : none
+*Description: change task state from ready to running,
+*			  remove the task from the ready list.
+*********************************************************************************/
+void OsSched_ReadyToRunning(
+							TaskType taskID
+							)
+{
+    OsSched_ReadyListRemoveFromHead(&OsTask_ReadyList[OsTask_TCBs[taskID].CurrentPriority]);
+    OsTask_TCBs[taskID].state = RUNNING;
+}
+
+
+/*******************************************************************************
+*Function Name: OsSched_WaitingToReady
+*Parameter (In): taskID		-id of the task to be moved from waiting to ready.
+*Parameter (Out): none
+*Parameter (In/Out): none
+*Return : none
+*Description: change task state from waiting to ready,
+*			  add the task to the tail the ready list.
+*********************************************************************************/
+#if( (OS_CONFORMANCE == OS_CONFORMANCE_ECC1) ||  (OS_CONFORMANCE == OS_CONFORMANCE_ECC2) )
+void OsSched_WaitingToReady(
+							TaskType taskID
+							)
+{
+	OsTask_TCBs[taskID].state=READY;
+	OsSched_ReadyListAddToTail(&OsTask_ReadyList[OsTask_TCBs[taskID].CurrentPriority], taskID);
+}
+#endif
+

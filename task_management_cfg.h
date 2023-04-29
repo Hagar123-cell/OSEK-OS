415cf61c352d58a8b5a328589900324e6f6727ba
David-Samir-Luis <85419503+David-Samir-Luis@users.noreply.github.com>
2023-04-29 02:13:23 +0200

    Add files via upload

---
+++ task_management_cfg.h
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *
+ * Module: TaskManagement_cfg
+ *
+ * File Name: taskManagement_cfg.h
+ *
+ * Description: Description: Pre-Compile Configuration Header file for Task Management
+ *
+ * Author:
+ *
+ *******************************************************************************/
+
+#ifndef TASK_MANAGEMENT_CFG_H_
+#define TASK_MANAGEMENT_CFG_H_
+
+#define OSTASK_NUMBER  20   /* 		1 .. 255		 */
+
+#define OSTASK_PRIORITY_LEVELS  8
+
+#define INVALID_TASK OSTASK_NUMBER
+#define OS_CONFORMANCE OS_CONFORMANCE_ECC2
+
+#endif /* TASK_MANAGEMENT_CFG_H_ */

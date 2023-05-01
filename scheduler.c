c6d1da9b66fda10775bc86b9bff3f871dfeac8dd
peter-samir-luis <petersamir06423852@gmail.com>
2023-05-01 02:08:29 +0200

    implement OsSched_getHighestReadyTask() function and declare OsTask_HighestPriority variable and update it in OsSched_SuspendedToReady and OsSched_WaitingToReady

--- scheduler.c
+++ scheduler.c
@@ -11,12 +11,39 @@
  *******************************************************************************/
 
 #include "scheduler.h"
-OsTask_TCBType              	OsTask_TCBs        [OSTASK_NUMBER]; /* TCB array for tasks based on task ID */
+OsTask_TCBType              		OsTask_TCBs        [OSTASK_NUMBER]; /* TCB array for tasks based on task ID */
 static Os_ReadyListType             OsTask_ReadyList           [OSTASK_PRIORITY_LEVELS]; /* array of ready lists based on priority */
-static TaskType                     OsTask_RunningTask;                         /* currently running task */
-static TaskType                     OsTask_RunningTaskNext;                    /*      task to be run next     */
+TaskType                     		OsTask_RunningTask;                         /* currently running task */
+TaskType                   		    OsTask_RunningTaskNext;                    /*      task to be run next     */
+static TaskType						OsTask_HighestPriority;		        	 /* the highest priority ready or running */
+ 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	/* if we activate a task that has priority higher than the running task */
+																		    /*but the running task is non-preemptive ,so store highest priority in this variable  */
 
 
+/*******************************************************************************
+ *                            static Function Prototypes 		               *
+ *******************************************************************************/
+static void OsSched_ReadyListAddToHead
+(
+		Os_ReadyListType* list,
+		TaskType 		  taskID
+);
+
+static void OsSched_ReadyListAddToTail
+(
+	 Os_ReadyListType* list,
+	 TaskType 	     taskID
+);
+
+static void OsSched_ReadyListRemoveFromHead
+(
+	Os_ReadyListType* list
+);
+
+static void OsSched_getHighestReadyTask
+(
+		TaskType *taskIDPtr
+);
 
 /*******************************************************************************
 *Function Name: OsSched_ReadyListInit
@@ -127,42 +154,6 @@ static void OsSched_ReadyListRemoveFromHead(
 }
 
 
-
-/*******************************************************************************
-*Function Name: OsSched_ReadyListRemoveFromHead
-*Parameter (In): list  	    -ready list to remove task from.
-*Parameter (Out): none
-*Parameter (In/Out): none
-*Return : none
-*Description: Remove the task at the head of the list .
-********************************************************************************/
-static void OsSched_ReadyListRemoveFromHead(
-											Os_ReadyListType* list
-										   )
-{
-	TaskType oldHead;
-
-	if(list->head == INVALID_TASK) /* list is already empty */
-	{
-		/* Do Nothing */
-	}
-	else if (list->head == list->tail ) /* there is only one task in list */
-	{
-		list->head = INVALID_TASK;
-		list->tail = INVALID_TASK;
-		/* Removed task next is already INVALID_TASK */
-	}
-	else  /* list contains more than one task */
-	{
-		oldHead=list->head;
-		list->head=OsTask_TCBs[oldHead].next;
-		OsTask_TCBs[oldHead].next=INVALID_TASK; /* assign Removed task next to INVALID_TASK */
-	}
-}
-
-
-
-
 /*******************************************************************************
 *Function Name: OsSched_RunningToReady
 *Parameter (In): taskID		-id of the task to be moved from running to ready.
@@ -188,11 +179,13 @@ void OsSched_RunningToReady(
 *Parameter (In/Out): none
 *Return : none
 *Description: change task state from Suspended to ready,
-*			  add the task to the tail of the ready list.
+*			  add the task to the tail of the ready list,
+*			  update OsTask_HighestPriority .
 *********************************************************************************/
-void OsSched_SuspendedToReady(
-								TaskType taskID
-								)
+void OsSched_SuspendedToReady
+(
+	 TaskType taskID
+)
 {
 /*
  * task release all resources and internal resources before termination (move to suspended state),
@@ -205,6 +198,15 @@ void OsSched_SuspendedToReady(
 #endif
     OsSched_ReadyListAddToTail(&OsTask_ReadyList[OsTask_TCBs[taskID].CurrentPriority], taskID);
     OsTask_TCBs[taskID].state    = READY; /* change state of the task to ready*/
+
+    if(OsTask_TCBs[taskID].CurrentPriority > OsTask_HighestPriority)
+    {
+    	OsTask_HighestPriority=OsTask_TCBs[taskID].CurrentPriority;
+    }
+    else
+    {
+    	/* Do Nothing */
+    }
 }
 
 /*******************************************************************************
@@ -232,15 +234,50 @@ void OsSched_ReadyToRunning(
 *Parameter (In/Out): none
 *Return : none
 *Description: change task state from waiting to ready,
-*			  add the task to the tail the ready list.
+*			  add the task to the tail the ready list,
+*			  update OsTask_HighestPriority .
 *********************************************************************************/
 #if( (OS_CONFORMANCE == OS_CONFORMANCE_ECC1) ||  (OS_CONFORMANCE == OS_CONFORMANCE_ECC2) )
-void OsSched_WaitingToReady(
-							TaskType taskID
-							)
+void OsSched_WaitingToReady
+(
+			TaskType taskID
+)
 {
 	OsTask_TCBs[taskID].state=READY;
 	OsSched_ReadyListAddToTail(&OsTask_ReadyList[OsTask_TCBs[taskID].CurrentPriority], taskID);
+
+	if(OsTask_TCBs[taskID].CurrentPriority > OsTask_HighestPriority)
+	{
+		OsTask_HighestPriority=OsTask_TCBs[taskID].CurrentPriority;
+	}
+	else
+	{
+		/* Do Nothing */
+	}
 }
 #endif
+
+
+/*******************************************************************************
+*Function Name: OsSched_getHighestReadyTask
+*Parameter (In): none
+*Parameter (Out): taskIDPtr		-pointer to store highest priority task ID.
+*Parameter (In/Out): none
+*Return : none
+*Description: search the ready list to get the highest priority task ID .
+*********************************************************************************/
+static void OsSched_getHighestReadyTask
+(
+		TaskType *taskIDPtr
+)
+{
+	sint16 i=(sint16)OsTask_HighestPriority; /* signed to exit when (i<0) otherwise the condition always true */
+	*taskIDPtr=INVALID_TASK;
+	for( ; (*taskIDPtr==INVALID_TASK) && (i>=0) ;i--)
+	{
+		*taskIDPtr =OsTask_ReadyList[i].head;
+	}
+
+}
+
 

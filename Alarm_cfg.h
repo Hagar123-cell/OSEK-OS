854f60dc20bc885140fa22528d998a83b803d4e4
yasmin afifi <yasminkhaledd2@gmail.com>
2023-05-02 17:05:36 +0300

    Created Alarm_Cfg.h file for Pre-Compile Config

---
+++ Alarm_cfg.h
@@ -0,0 +1,20 @@
+ /******************************************************************************
+ *
+ * Module: Alarm
+ *
+ * File Name: Alarm_Cfg.h
+ *
+ * Description: Pre-Compile Configuration Header file for Alarm management
+ *
+ * Author: Yasmin Afifi
+ ******************************************************************************/
+
+#ifndef ALARM_CFG_H
+#define ALARM_CFG_H
+
+
+#define No_ALARMS             (2U)
+
+
+
+#endif /* ALARM_CFG_H */

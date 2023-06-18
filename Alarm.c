7723184f04572d6680096aaa4b65af9be25dd112
yasmin afifi <yasminkhaledd2@gmail.com>
2023-06-18 03:09:46 +0300

    update APIs description

--- Alarm.c
+++ Alarm.c
@@ -276,6 +276,16 @@ StatusType CancelAlarm ( AlarmType AlarmID )
 
 }
 
+/************************************************************************************
+* Service Name: IncrementCounter
+* Service ID[hex]: 
+* Sync/Async: 
+* Reentrancy: 
+* Parameters (in): AlarmID - Reference to alarm 
+*                  Increment_value - Value we will be using 
+* Parameters (out): minimal increment
+* Description: The system service used to increment the counter.
+************************************************************************************/
 #if OSALARM_NUMBER_OF_ALARMS 
 TickType IncrementCounter(AlarmType AlarmID, TickType Increment_value){
 
@@ -335,8 +345,16 @@ TickType IncrementCounter(AlarmType AlarmID, TickType Increment_value){
 #endif
 
 
+/************************************************************************************
+* Service Name: Alarm_init
+* Service ID[hex]: 
+* Sync/Async: 
+* Reentrancy: 
+* Parameters (in): None
+* Parameters (out): None
+* Description: The system service used to initialize the alarm.
+************************************************************************************/
 #if OSALARM_NUMBER_OF_ALARMS 
-
 void Alarm_init(void)
 {
 
@@ -357,8 +375,18 @@ for(AlarmID = 0; AlarmID < OSALARM_NUMBER_OF_ALARMS; AlarmID++){
 }
 #endif
 
-#if OSALARM_NUMBER_OF_ALARMS
 
+/************************************************************************************
+* Service Name: IncrementAlarm
+* Service ID[hex]: 
+* Sync/Async: 
+* Reentrancy: 
+* Parameters (in): AlarmID - Reference to alarm 
+*                  Increment_value - Value we will be using 
+* Parameters (out): Rest of increment
+* Description: The system service used to increment the alarm.
+************************************************************************************/
+#if OSALARM_NUMBER_OF_ALARMS
 TickType IncrementAlarm(AlarmType AlarmID, TickType Increment_value){
 
   TickType RestIncrement;

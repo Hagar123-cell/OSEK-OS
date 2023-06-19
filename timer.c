098ee9a07e1d4e0d5d69dcbced56d2d3b0acf080
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2023-06-19 19:38:44 +0300

    Update timer.c

--- timer.c
+++ timer.c
@@ -2,24 +2,27 @@
 #include "Alarm.h"
 void timer_init(void)
 {
-	SET_BIT(CNRTR,1);           //enable timer
+	           
 	SET_BIT(CNRTR,2);          // select mode
 	SET_BIT(CNRTR,3);         /* select suitable prescalar
                             */
 	CLR_BIT(CNRTR,4);
 	return void;
 }
-void compare(int compare_value)
+void Enable_timer()
 {
-    if(compare_value == CNT)
-    {
-        INTERRUPT = 1;
-    }
-    else
-    {
-        //no thing
-    }
+	SET_BIT(CNRTR,1);   
+	/***********enable timer***************/
+	return void;
+}
+void Disable_timer(void)
+{
+	
+	CLR_BIT(CNRTR,1);
+	/***********disable timer***************/
+	return void;
 
+}
 }
 void ISR(void)
 {

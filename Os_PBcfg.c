de3fe4e8e8ab260ccb632aeeb454d918c730bdb5
NoranNagdi <noranagdi33@gmail.com>
2023-06-24 04:36:57 +0300

    added main file

--- Os_PBcfg.c
+++ Os_PBcfg.c
@@ -1,5 +1,22 @@
 #include "Os.h"
 
-OsTask * OsTaskConfig;
+#define no_of_tasks 2
+#define STACKSIZE 240
 
+uint32 task1Stack [STACKSIZE];
+uint32 task2Stack [STACKSIZE];
+
+void Task1 (void);
+void Task2 (void);
+
+typedef struct Task_ConfigType
+{
+	OsTask tasksConfig[no_of_tasks];
+} Task_ConfigType;
+
+const Task_ConfigType Task_Configuration = {
+    &task1Stack[STACKSIZE],STACKSIZE,Task1,EXTENDED,10,2,FULL,0,AUTOMATIC,
+
+    &task2Stack[STACKSIZE],STACKSIZE,Task2,EXTENDED,10,1,FULL,0,AUTOMATIC
+};
 

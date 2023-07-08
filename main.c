2c0ad5f97b05f079d486d38f556922a045262f7b
David-Samir-Luis <Davidsamir07@gmail.com>
2023-07-08 16:36:36 +0300

    update main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "Os.h"
+#define TASK1_ID 0
+#define TASK2_ID 1
 
 uint32 ledOn = 0 , ledToggle = 0;
 
@@ -17,7 +19,7 @@ void Task1 (void)
   {
     BLUE_LED_ON();  
     for(uint32 i=0; i<1000; i++);
-    TerminateTask();
+    ActivateTask(TASK2_ID);
   }
 }
 
@@ -27,7 +29,7 @@ void Task2 (void)
   {
     BLUE_LED_TOGGLE();
     for(uint32 i=0; i<1000; i++);
-    ActivateTask(0);
+    TerminateTask();
   }
 }
 

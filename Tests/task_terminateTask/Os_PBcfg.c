02494b5c4bcbb2f5f5f074ec15ee91a6b6222364
David-Samir-Luis <85419503+David-Samir-Luis@users.noreply.github.com>
2023-07-12 02:57:16 +0300

    Update Os_PBcfg.c

--- Tests/task_terminateTask/Os_PBcfg.c
+++ Tests/task_terminateTask/Os_PBcfg.c
@@ -2,16 +2,14 @@
 
 uint32 task1Stack [STACKSIZE];
 uint32 task2Stack [STACKSIZE];
-uint32 task3Stack [STACKSIZE];
-uint32 task4Stack [STACKSIZE];
-uint32 task5Stack [STACKSIZE];
+uint32 IdleTaskStack [STACKSIZE];
+
 
 
 const  Task_ConfigType Task_Configuration = {
-     task1Stack,STACKSIZE,Task1,EXTENDED,0,6,FULL,0,TRUE,
-	 task2Stack,STACKSIZE,Task2,EXTENDED,0,2,NON,0,FALSE,
-	 task3Stack,STACKSIZE,Task3,EXTENDED,0,3,FULL,0,FALSE,
-	 task4Stack,STACKSIZE,Task4,BASIC,3,4,FULL,0,FALSE,
-	 task5Stack,STACKSIZE,Task5,BASIC,2,5,FULL,0,FALSE
+     task1Stack,STACKSIZE,Task1,BASIC,4,1,FULL,0,TRUE,
+     task2Stack,STACKSIZE,Task2,EXTENDED,0,2,NON,0,TRUE,
+     IdleTaskStack,STACKSIZE,IdleTask,BASIC,0,0,FULL,0,TRUE
+	 
 };
 

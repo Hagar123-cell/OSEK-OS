0aac68e6a185d174b1de20fd5f79b1f9e72e0126
David-Samir-Luis <85419503+David-Samir-Luis@users.noreply.github.com>
2023-07-12 02:58:53 +0300

    Update Os_PBcfg.h

--- Tests/task_terminateTask/Os_PBcfg.h
+++ Tests/task_terminateTask/Os_PBcfg.h
@@ -2,7 +2,7 @@
  * Os_PBcfg.h
  *
  *  Created on: Jul 7, 2023
- *      Author: Peter
+ *      Author: David
  */
 
 #ifndef OSEK_OS_OS_PBCFG_H_
@@ -19,9 +19,9 @@ typedef struct Task_ConfigType
 extern const  Task_ConfigType Task_Configuration ;
 void Task1 (void);
 void Task2 (void);
-void Task3 (void);
-void Task4 (void);
-void Task5 (void);
+void IdleTask (void);
+
+
 
 
 #endif /* OSEK_OS_OS_PBCFG_H_ */

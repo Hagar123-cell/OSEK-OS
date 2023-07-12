87a6cbf59dc7892d6f14f42d5d47a5c1d3760f21
peter-samir-luis <68796155+peter-samir-luis@users.noreply.github.com>
2023-07-13 01:01:56 +0300

    Create Os_PBcfg.h

---
+++ Tests/task_GetTaskID/Os_PBcfg.h
@@ -0,0 +1,25 @@
+/*
+ * Os_PBcfg.h
+ *
+ *  Created on: Jul 7, 2023
+ *      Author: Peter
+ */
+
+#ifndef OSEK_OS_OS_PBCFG_H_
+#define OSEK_OS_OS_PBCFG_H_
+#include "Os.h"
+#define STACKSIZE 240
+
+typedef struct Task_ConfigType
+{
+	OsTask tasksConfig[OSTASK_NUMBER_OF_TASKS];
+} Task_ConfigType;
+
+
+extern const  Task_ConfigType Task_Configuration ;
+void Task1 (void);
+
+
+
+
+#endif /* OSEK_OS_OS_PBCFG_H_ */
